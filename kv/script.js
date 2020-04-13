6c74e7ad5d56cbd63de4c6384034afaeb8c1538c
tarasstefanyuk <60108539+tarasstefanyuk@users.noreply.github.com>
2020-04-13 22:13:38 +0300

    Update script.js

--- kv/script.js
+++ kv/script.js
@@ -1,11 +1,7 @@
 const table = document.getElementById('table');
 const header = document.querySelector('header');
-
 var now = new Date();
-
 header.innerHTML ="Курс валют на: "+ now.getHours()+':'+now.getMinutes()+':'+now.getSeconds();
-
-
 let requestURL = 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5';
 let request = new XMLHttpRequest();
 request.open('GET', requestURL);
@@ -15,17 +11,7 @@ request.send();
 request.onload = function(){
 	const curs = request.response;
 	const curss = JSON.parse(curs);
-
-
-
-
-
-
-
-
-  // document.body.append(curs);
 	addHeader(curss);
-	// showHerroes(superHerroes);
 };
 
 function addHeader(json){
@@ -43,15 +29,7 @@ function addHeader(json){
 	td2.innerHTML += json[key].buy;
 	tr.append(td3);
 	td3.innerHTML += json[key].sale;
-	
-
-	// let td = document.createElement('td');
-
-
-
-	
-  // tr.innerHTML += json[key].ccy;
-  
+	  
 }
 }
 
